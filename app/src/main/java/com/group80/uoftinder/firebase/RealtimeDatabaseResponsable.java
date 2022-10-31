abf3ce3e068c4854623c4622b273707a6126dd8d
Lance Li <46139702+LanceLi1416@users.noreply.github.com>
2022-10-31 15:15:22 -0400

    Add the 2 interfaces

---
+++ app/src/main/java/com/group80/uoftinder/firebase/RealtimeDatabaseResponsable.java
@@ -0,0 +1,5 @@
+package com.group80.uoftinder.firebase;
+
+public interface RealtimeDatabaseResponsable {
+
+}
---
+++ app/src/main/java/com/group80/uoftinder/firebase/StorageDatabaseResponsable.java
@@ -0,0 +1,4 @@
+package com.group80.uoftinder.firebase;
+
+public interface StorageDatabaseResponsable {
+}
