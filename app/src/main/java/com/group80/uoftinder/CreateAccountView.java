ee6defae6dadc4c6ab7a5d82bc7833f7bd0f2d0a
Milena <milena.mijuskovic1115@gmail.com>
2022-11-09 18:36:34 -0500

    editing CreateAccountView.java with a simple example of entering a user information for registration

---
+++ app/src/main/java/com/group80/uoftinder/CreateAccountView.java
@@ -0,0 +1,23 @@
+package com.group80.uoftinder;
+
+import java.util.HashMap;
+
+public class CreateAccountView {
+    //very rough class right now, just using this to store questions and store answers into User
+    //    String[] BasicQuestions = {"Name", "Age", "Type", "Gender"};
+    HashMap<String, String> basicInfo = new HashMap<String, String>();
+//    String name = "Milena Mijuskovic";
+//
+    //UI
+    // example
+    String email = "milena.mijuskovic@somegmail.com"; //entry field
+
+    String age = "19"; //entry field
+    String type = "academic"; //choose from academic, romantic, friendship
+    String gender = "Female"; //choose from female, male, non-binary, prefer not to answer/other
+
+
+
+
+
+}
