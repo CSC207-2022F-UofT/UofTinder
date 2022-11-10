91eac38c0ca90a3086eca9a28bd420e0cd892054
swaner2 <j.chen1904@gmail.com>
2022-11-09 19:06:31 -0500

    created stuff to test with

--- app/src/main/java/com/group80/uoftinder/entities/User.java
+++ app/src/main/java/com/group80/uoftinder/entities/User.java
@@ -22,7 +22,7 @@ public class User{
 
     private ArrayList answers;
 
-    User(String email, String password){
+    public User(String email, String password){
         this.email = email;
         this.password = password;
     }
