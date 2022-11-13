bb9e6358e39950206dc1b4a6313403b8fcd4f64c
swaner2 <j.chen1904@gmail.com>
2022-11-13 18:11:24 -0500

    merging

--- app/src/main/java/com/group80/uoftinder/UserAccountController.java
+++ app/src/main/java/com/group80/uoftinder/UserAccountController.java
@@ -1,5 +1,7 @@
 package com.group80.uoftinder;
 
+import com.group80.uoftinder.create_account_use_case.CreateAccountInteractor;
+
 public class UserAccountController {
 
     public boolean newAccount(String email, String password1, String password2) {
