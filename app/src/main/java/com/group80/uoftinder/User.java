dbf525e80816d3e0d938661a2c5dca10ea6ac56a
Chloe Lam <chloelam2407@gmail.com>
2022-11-14 15:37:44 -0500

    add user class

--- app/src/main/java/com/group80/uoftinder/User.java
+++ app/src/main/java/com/group80/uoftinder/User.java
@@ -1,43 +1,110 @@
 package com.group80.uoftinder;
 
+import android.net.Uri;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 public class User {
-    private String name;
+
+    private String uid;
+
+    private String userType; // make this final later idk
+
+    private int score;
+
+    // this information is displayed on profile
+    private String displayName; // last name and first name displayed on profile
+    private Uri photoUrl;
     private int age;
-    private String location;
-    private String bio;
-    private String profilePicture;
+    private String gender;
 
-    public User() {}
+    private HashSet<Integer>[] answers;
+    private List viewed;
+    private List liked;
+    private List matches;
 
-    public User(String profilePicture, String name, int age, String location, String bio) {
-        this.profilePicture = profilePicture;
-        this.name = name;
+    public User(){
+        this.liked = new ArrayList();
+        this.viewed = new ArrayList();
+        this.matches = new ArrayList();
+    }
+
+    public String getUserType(){
+        return userType;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+    public int getScore() {
+        return score;
+    }
+
+    public void setAge(int age) {
         this.age = age;
-        this.location = location;
-        this.bio = bio;
-
     }
-    
-    public String getProfilePicture() {
-        return profilePicture;
-    }
-
     public int getAge() {
         return age;
     }
 
-    public String getName() {
-        return name;
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public String getGender() {
+        return gender;
     }
 
-    public String getBio() {
-        return bio;
+    public Uri getPhotoUrl() {
+        return photoUrl;
     }
 
-    public String getLocation() {
-        return location;
+    public void setPhotoUrl(Uri photoUrl) {
+        this.photoUrl = photoUrl;
     }
 
-    public void addToList(User displayedUser, boolean b) {
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public HashSet<Integer>[] getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(HashSet<Integer>[] answers) {
+        this.answers = answers;
+    }
+
+    public List getViewed() {
+        return viewed;
+    }
+
+    public void setViewed(List viewed) {
+        this.viewed = viewed;
+    }
+
+    public List getLiked() {
+        return liked;
+    }
+
+    public void setLiked(List liked) {
+        this.liked = liked;
+    }
+
+    public List getMatches() {
+        return matches;
+    }
+
+    public void setMatches(List matches) {
+        this.matches = matches;
     }
 }
