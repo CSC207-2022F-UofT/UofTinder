b7eeaa0eea265bfd759e9460daa91a500b4dd44f
swaner2 <j.chen1904@gmail.com>
2022-11-14 14:21:43 -0500

    edited User

--- app/src/main/java/com/group80/uoftinder/entities/User.java
+++ app/src/main/java/com/group80/uoftinder/entities/User.java
@@ -22,14 +22,9 @@ public class User{
 
     private ArrayList answers;
 
-    User(String email, String password){
+    public User(String email, String password){
         this.email = email;
         this.password = password;
-    }
-
-
-    public String getEmail() {
-        return email;
     }
 
     public String getUserType(){
