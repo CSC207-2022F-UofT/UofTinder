507138f2596c1987600632c709d3602c35b7e6c9
Nicholas Poon <47435386+nicholaspoon03@users.noreply.github.com>
2022-11-14 14:19:46 -0500

    working on GenerateCompatibilityList init method

--- app/src/main/java/com/group80/uoftinder/GenerateCompatibilityList.java
+++ app/src/main/java/com/group80/uoftinder/GenerateCompatibilityList.java
@@ -2,12 +2,17 @@ package com.group80.uoftinder;
 import java.util.ArrayList;
 
 public class GenerateCompatibilityList {
+    private User curUser;
     private ArrayList<User> compatibilityList;
     private RecOutputBoundary recOutputBoundary;
 
     public GenerateCompatibilityList {
         // use User.getScore() in a loop that iterates over all the users
         // to retrieve User scores and compute compatibility into a list
+        for (User user : User.getAllUsers()) {
+            userScore = user.getScore();
+            
+        }
     }
 
     public void update() {
