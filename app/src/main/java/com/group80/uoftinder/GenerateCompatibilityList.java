96bda88bdf2150d4503ad2d0ecf7cbd3eacff3d7
Nicholas Poon <47435386+nicholaspoon03@users.noreply.github.com>
2022-11-14 16:24:40 -0500

    finishing generate compatibility list functionality

--- app/src/main/java/com/group80/uoftinder/GenerateCompatibilityList.java
+++ app/src/main/java/com/group80/uoftinder/GenerateCompatibilityList.java
@@ -1,18 +1,62 @@
 package com.group80.uoftinder;
+import android.icu.text.UFormat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.auth.User;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class GenerateCompatibilityList {
     private User curUser;
     private ArrayList<User> compatibilityList;
     private RecOutputBoundary recOutputBoundary;
+    private List<User> users;
 
-    public GenerateCompatibilityList {
-        // use User.getScore() in a loop that iterates over all the users
-        // to retrieve User scores and compute compatibility into a list
-        for (User user : User.getAllUsers()) {
-            userScore = user.getScore();
-            
+    //TODO: use API
+    // Navigate all users
+    // RelatimDbControllder realtimeDbController = ...;
+    // List<Users> realtimeDbController.getAllUsers(Void unused);
+    //TODO: end use API
+
+    public GenerateCompatibilityList() {
+        RealtimeDbController realtimeDbController = ...;
+        List<User> users = realtimeDbController.getAllUsers(Void unused, type=curUser.getUserType());
+    }
+
+    public void generateCompatibilityList() {
+        ArrayList<Integer> compScores = calculateCompatibilityScores(users);
+        for (int i = 0 ; i < users.size() ; i++) {
+            int maxIndex = getMaxIndex(compScores);
+            compatibilityList.add(users.get(maxIndex));
+            compScores.remove(maxIndex);
+            users.remove(maxIndex);
         }
+        recOutputBoundary.displayCompatibleUsers(compatibilityList);
+    }
+
+    public int getMaxIndex(ArrayList<Integer> lst) {
+        int max = 0;
+        int maxIndex = 0;
+        for (int i = 0; i < len(lst); i++) {
+            if (lst.get(i) > max) {
+                max = lst.get(i);
+                maxIndex = i;
+            }
+        }
+
+        return maxIndex;
+    }
+
+    public ArrayList<Integer> calculateCompatibilityScores(List<User> users) {
+        ArrayList<Integer> compScores = new ArrayList<>();
+        for (User user : users) {
+            compScores.add(user.getScore() & curUser.getScore());
+        }
+
+        return compScores;
     }
 
     public void update() {
--- app/src/main/java/com/group80/uoftinder/RecommendationPresenter.java
+++ app/src/main/java/com/group80/uoftinder/RecommendationPresenter.java
@@ -3,11 +3,10 @@ package com.group80.uoftinder;
 import java.util.ArrayList;
 
 public class RecommendationPresenter implements RecOutputBoundary{
-    private User currentUser;
 
     public void displayCompatibleUsers(ArrayList<User> compatibilityList) {
         if (compatibilityList.size() > 0) {
-            // display compatibilityList[0]
+            // display compatibilityList.get(0)
         }
         else {
             // display No Compatible User Error Message
