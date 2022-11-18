9fcd2f4235dd6ba189104b53c92d2fb7e930265e
Chloe Lam <chloelam2407@gmail.com>
2022-11-18 16:44:21 -0500

    add interface and made helper methods for button click

---
+++ app/src/main/java/com/group80/uoftinder/RecommendationView.java
@@ -0,0 +1,75 @@
+package com.group80.uoftinder;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class RecommendationView extends AppCompatActivity implements RecommendationViewInterface {
+    private User current;
+
+    // create variables for all elements that are displayed
+    ImageView profilePicture;
+    TextView name;
+    TextView gender;
+    TextView age;
+    Button noButton;
+    Button yesButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // set the view to recommendation profile display
+        setContentView(R.layout.recommendation_profile_display);
+
+        // connect all the different components of the screen
+        profilePicture = findViewById(R.id.profilePicture);
+        name = findViewById(R.id.name);
+        gender = findViewById(R.id.gender);
+        age = findViewById(R.id.age);
+        noButton = findViewById(R.id.noButton);
+        yesButton = findViewById(R.id.yesButton);
+
+        // initialize first user
+        RecControllerPresenter recControllerPresenter = new RecControllerPresenter();
+        User displayedUser = recControllerPresenter.displayNextUser();
+
+        // yes button click listener
+        yesButton.setOnClickListener(view -> {
+            buttonClick(displayedUser, true);
+        });
+
+        // no button click listener
+        noButton.setOnClickListener(view -> {
+            buttonClick(displayedUser, false);
+
+        });
+    }
+
+    private void buttonClick(User displayedUser, boolean liked) {
+        // add displayed User to viewed/liked list
+        current.addToList(displayedUser, liked);
+        // displays next user
+        RecControllerPresenter recControllerPresenter = new RecControllerPresenter();
+        recControllerPresenter.displayNextUser();
+
+    }
+    // set information on screen to the displayed User's information
+    public void displayUser(User displayedUser) {
+        profilePicture.setImageURI(displayedUser.getPhotoUrl());
+        name.setText(displayedUser.getDisplayName());
+        age.setText(Integer.toString(displayedUser.getAge()));
+        gender.setText(displayedUser.getGender());
+    }
+
+    public void noCompatibleUser() {
+        // display a new screen that says no more users left!
+        final Context context = this;
+        Intent intent = new Intent(context, NoNewRecommendation.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/group80/uoftinder/RecommendationViewInterface.java
@@ -0,0 +1,10 @@
+package com.group80.uoftinder;
+
+public interface RecommendationViewInterface {
+
+    // display user
+    void displayUser(User displayedUser);
+    // no more compatible users
+    void noCompatibleUser();
+
+}
