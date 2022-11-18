f6c804e101475023a2a38f6dbaf1e3267714bb17
Lance Li <46139702+LanceLi1416@users.noreply.github.com>
2022-11-18 18:26:25 -0500

    Finish Chat System (#21)
    * Add the 2 interfaces
    
    * essential `Firebase` setup
    
    * image upload functioning
    
    * basic download functioning
    
    * update java docs for existing code
    
    * realtime basic setup
    
    * Finish chat feature
    
    * wrap up image upload / download into a facade
    
    * add javadocs

--- app/src/main/java/com/group80/uoftinder/HelloWorld.java
+++ app/src/main/java/com/group80/uoftinder/HelloWorld.java
@@ -1,14 +1,24 @@
 package com.group80.uoftinder;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class HelloWorld extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hello_world);
+
+        Button button = findViewById(R.id.helloWorldEnterChatButton);
+        button.setOnClickListener(view -> {
+            Intent intent = new Intent(HelloWorld.this, ChatActivity.class);
+            // TODO: remove such dependency
+            intent.putExtra("name", "Bot");
+            intent.putExtra("contactUid", "FJuPu9PeQ8TpTPZmDXOVluUCp7c2");
+            startActivity(intent);
+        });
     }
 }
