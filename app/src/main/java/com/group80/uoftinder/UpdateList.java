7a52d3bb05f32cd3f29ded4d05a4906ec92168aa
Chloe Lam <chloelam2407@gmail.com>
2022-11-18 15:23:39 -0500

    add method that writes to user like and view list to firebase

--- app/src/main/java/com/group80/uoftinder/UpdateList.java
+++ app/src/main/java/com/group80/uoftinder/UpdateList.java
@@ -1,23 +1,34 @@
 package com.group80.uoftinder;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class UpdateList extends User {
     private User displayedUser;
     private boolean liked;
-    private ArrayList<User> viewedList;
-    private ArrayList<User> likedList;
+    private List<User> viewedList;
+    private List<User> likedList;
 
     public UpdateList(User displayedUser, boolean liked) {
         super();
         this.displayedUser = displayedUser;
         this.liked = liked;
+        this.viewedList = currentUser.getViewed();
+        this.likedList = currentUser.getLiked();
     }
     public void addToList(User displayedUser, boolean liked) {
         viewedList.add(displayedUser);
+        currentUser.setViewed(viewedList);
+        // push currentUser.getViewed() to firebase viewed list
         if (liked) {
             likedList.add(displayedUser);
+            currentUser.setLiked(likedList);
+            // push currentUser.getLiked() to firebase liked list
         }
+        // sending display user id to be added to firebase
+        // if liked is true, push to liked list and viewed list
+        // if liked is false, push to viewed list
+        updateListStatus(displayedUser.getUid(), liked);
     }
 
 
