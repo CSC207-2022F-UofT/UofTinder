1eb9c1b0b75810c3c7ea14d56166bcbb63f6be9d
VG2800 <goelvedant28@gmail.com>
2022-11-18 19:59:58 -0500

    realtime db updates, wip

--- app/src/main/java/com/group80/uoftinder/HelloWorld.java
+++ app/src/main/java/com/group80/uoftinder/HelloWorld.java
@@ -1,8 +1,17 @@
 package com.group80.uoftinder;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.group80.uoftinder.firebase.realtime.User;
+import com.group80.uoftinder.firebase.realtime.UserRealtimeDbFacade;
+
+import java.util.List;
 
 public class HelloWorld extends AppCompatActivity {
 
@@ -10,5 +19,23 @@ public class HelloWorld extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hello_world);
+
+        UserRealtimeDbFacade.getAllUsers("academic");
+
+        User user1 = new User("Lance", "Li", "lisinan2", "academic");
+        UserRealtimeDbFacade.uploadUser(user1);
+        User user2 = new User("Vedant", "Goel", "vgvg", "academic");
+        UserRealtimeDbFacade.uploadUser(user2);
+
+        int counter = 0;
+        List<User> allUsers = UserRealtimeDbFacade.getAllUsers("academic");
+
+        for (User user : allUsers) {
+            Log.d(TAG, user.getUid());
+            counter++;
+        }
+
+        TextView textView = findViewById(R.id.helloWorldTextEdit);
+        textView.setText(Integer.toString(counter));
     }
 }
