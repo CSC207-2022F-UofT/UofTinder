657f9019930ccb2c78127688500cefb13affdb95
swaner2 <j.chen1904@gmail.com>
2022-11-18 19:13:59 -0500

    created Login use case

--- app/src/main/java/com/group80/uoftinder/HelloWorld.java
+++ app/src/main/java/com/group80/uoftinder/HelloWorld.java
@@ -1,17 +1,10 @@
 package com.group80.uoftinder;
 
-import static android.content.ContentValues.TAG;
+import android.content.Intent;
+import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -32,35 +25,11 @@ public class HelloWorld extends AppCompatActivity {
     @Override
     public void onStart() {
         super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-//        if(currentUser != null){
-//            reload();
-//        }
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user == null) {
+            startActivity(new Intent(HelloWorld.this, Login.class));
+        }
+
     }
 
-    private void createAccount(String email, String password) {
-        // [START create_user_with_email]
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(HelloWorld.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI(null);
-                        }
-                    }
-
-                    private void updateUI(FirebaseUser user) {
-                    }
-                });
-}
 }
--- app/src/main/java/com/group80/uoftinder/Login.java
+++ app/src/main/java/com/group80/uoftinder/Login.java
@@ -3,6 +3,7 @@ package com.group80.uoftinder;
 import static android.content.ContentValues.TAG;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -19,8 +20,8 @@ import com.google.firebase.auth.FirebaseUser;
 public class Login extends AppCompatActivity {
     private FirebaseAuth mAuth;
 
-    String email = ((EditText) findViewById(R.id.email)).getText().toString().trim();
-    String password = ((EditText) findViewById(R.id.password)).toString().trim();
+    EditText loginEmail;
+    EditText loginPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,20 +29,35 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.loginview);
 
         mAuth = FirebaseAuth.getInstance(); // initialize the Firebase Auth
+        loginEmail = findViewById(R.id.email);
+        loginPassword = findViewById(R.id.password);
+
     }
 
     @Override
     public void onStart() {
         super.onStart();
+
+        String email = loginEmail.getText().toString().trim();
+        String password = loginPassword.getText().toString().trim();
         // Check if user is signed in (non-null) and update UI accordingly.
+
         FirebaseUser currentUser = mAuth.getCurrentUser(); // this is not null ONLY if you have logged in
         /* To log-in, use the following
          * mAuth.signInWithEmailAndPassword("demo@email.com", "password");
          * To get user uid, after you have signed in, do the following
          * mAuth.getCurrentUser().getUid();
          */
-        if(currentUser != null){
+        if(currentUser != null){ // this means the user is logged in
             currentUser.reload();
+        }
+        else if (TextUtils.isEmpty(email)) {
+            loginEmail.setError("Email is required!");
+            loginEmail.requestFocus();
+        }
+        else if (TextUtils.isEmpty(password)){
+            loginPassword.setError("Password is required!");
+            loginPassword.requestFocus();
         }
         else {
             mAuth.signInWithEmailAndPassword(email, password)
@@ -52,7 +68,9 @@ public class Login extends AppCompatActivity {
                                 // Sign in success, update UI with the signed-in user's information
                                 Log.d(TAG, "signInWithEmail:success");
                                 FirebaseUser user = mAuth.getCurrentUser();
-                                mAuth.getCurrentUser().getUid();
+                                // updateUI(user);
+                                user.getUid();
+                                // getUserById()
 
                             } else {
                                 // If sign in fails, display a message to the user.
