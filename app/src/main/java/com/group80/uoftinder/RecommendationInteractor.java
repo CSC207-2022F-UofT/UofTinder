4d766e0400c0bba0693fa1a92d65048654989489
Helen Li <hallenljy@gmail.com>
2022-11-18 23:48:32 -0500

    integrate getUserById

--- app/src/main/java/com/group80/uoftinder/RecommendationInteractor.java
+++ app/src/main/java/com/group80/uoftinder/RecommendationInteractor.java
@@ -1,12 +1,11 @@
 package com.group80.uoftinder;
 
+import android.util.Log;
+
 import com.group80.uoftinder.entities.User;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 public class RecommendationInteractor {
@@ -19,21 +18,26 @@ public class RecommendationInteractor {
 
     }
 
+
     public void filterCompatibilityList(List<Set<Integer>> filters, int minAge, int maxAge) {
         for(String userId: compatibilityList) {
-            User user = UserRealtimeDbFacade.getUser(userId);
-            List<Set<Integer>> answers = user.getAnswers();
-            for(int i = 0; i < answers.size(); i++) {
-                Set<Integer> currentFilter = filters.get(i);
-                Set<Integer> currentAnswers = answers.get(i);
-                boolean shouldRemove = true;
-                for(Integer filterVal: currentFilter) {
-                    if(currentAnswers.contains(filterVal))
-                        shouldRemove = false;
+            UserRealtimeDbFacade.getUserById(userId, new UserCallback() {
+                @Override
+                public void onUser(User user) {
+                    List<Set<Integer>> answers = user.getAnswers();
+                    for(int i = 0; i < answers.size(); i++) {
+                        Set<Integer> currentFilter = filters.get(i);
+                        Set<Integer> currentAnswers = answers.get(i);
+                        boolean shouldRemove = true;
+                        for(Integer filterVal: currentFilter) {
+                            if(currentAnswers.contains(filterVal))
+                                shouldRemove = false;
+                        }
+                        if(shouldRemove || minAge > user.getAge() || user.getAge() > maxAge)
+                            compatibilityList.remove(i);
+                    }
                 }
-                if(shouldRemove || minAge > user.getAge() || user.getAge() > maxAge)
-                    compatibilityList.remove(i);
-            }
+            });
         }
         currentUser.setCompatibilityList(compatibilityList);
     }
