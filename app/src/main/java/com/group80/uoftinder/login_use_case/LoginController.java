b2d8d0ed40d0cc1aefa0d816f5b97f76d8b9935c
swaner2 <j.chen1904@gmail.com>
2022-11-20 14:37:59 -0500

    Deleted extends AppCompactActivity from LoginController, and deleted LoginUser since it was never used

--- app/src/main/java/com/group80/uoftinder/login_use_case/LoginController.java
+++ app/src/main/java/com/group80/uoftinder/login_use_case/LoginController.java
@@ -2,11 +2,9 @@ package com.group80.uoftinder.login_use_case;
 
 import android.widget.EditText;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.firebase.auth.FirebaseAuth;
 
-public class LoginController extends AppCompatActivity {
+public class LoginController {
     final LoginInput loginInput;
 
     public LoginController(LoginInput loginGateway) {
