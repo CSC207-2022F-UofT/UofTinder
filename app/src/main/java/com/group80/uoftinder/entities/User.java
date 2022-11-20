8424ad876f99606982488692cdd034899e464944
swaner2 <j.chen1904@gmail.com>
2022-11-19 21:24:11 -0500

    Merged with branch 0c for User constructor to take in a string parameter for uid.

--- app/src/main/java/com/group80/uoftinder/entities/User.java
+++ app/src/main/java/com/group80/uoftinder/entities/User.java
@@ -80,10 +80,6 @@ public class User {
         this.name = name;
     }
 
-    public void setUid(String uid) {
-        this.uid = uid;
-    }
-
     public String getUid() {
         return uid;
     }
--- app/src/main/java/com/group80/uoftinder/login_use_case/LoginInteractor.java
+++ app/src/main/java/com/group80/uoftinder/login_use_case/LoginInteractor.java
@@ -3,12 +3,8 @@ package com.group80.uoftinder.login_use_case;
 import android.text.TextUtils;
 import android.widget.EditText;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginInteractor extends AppCompatActivity implements LoginInput{
@@ -23,11 +19,6 @@ public class LoginInteractor extends AppCompatActivity implements LoginInput{
         String email = loginEmail.getText().toString().trim();
         String password = loginPassword.getText().toString().trim();
 
-        /* To log-in, use the following
-         * mAuth.signInWithEmailAndPassword("demo@email.com", "password");
-         * To get user uid, after you have signed in, do the following
-         * mAuth.getCurrentUser().getUid();
-         */
         if (TextUtils.isEmpty(email)) { // no email input, user == null
             loginPresenter.prepareFailureView(loginEmail, "Email is required!");
         }
@@ -37,19 +28,15 @@ public class LoginInteractor extends AppCompatActivity implements LoginInput{
 
         else { // user == null, signing in with email and password
             mAuth.signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            // LoginInteractor checks
+                    .addOnCompleteListener(this, task -> {
 
-                            // Login Presenter?
-                            if (task.isSuccessful()) {
-                                loginPresenter.prepareSuccessView(task, mAuth.getCurrentUser());
+                        // Login Presenter
+                        if (task.isSuccessful()) {
+                            loginPresenter.prepareSuccessView(task, mAuth.getCurrentUser());
 
-                            } else {
-                                // If sign in fails, display a message to the user.
-                                loginPresenter.prepareFailureViewLogin("Login Unsuccessful :(");
-                            }
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            loginPresenter.prepareFailureViewLogin("Login Unsuccessful :(");
                         }
                     });
         }
