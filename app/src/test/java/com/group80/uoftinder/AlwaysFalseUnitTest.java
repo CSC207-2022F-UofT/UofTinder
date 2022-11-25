63ecd77e93cd6a8916b773b50102f119acb666a0
Lance Li <46139702+LanceLi1416@users.noreply.github.com>
2022-11-24 19:39:18 -0500

    add a test that fails

---
+++ app/src/test/java/com/group80/uoftinder/AlwaysFalseUnitTest.java
@@ -0,0 +1,12 @@
+package com.group80.uoftinder;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class AlwaysFalseUnitTest {
+    @Test
+    public void test_alwaysFalse() {
+        fail("This test must fail!");
+    }
+}
