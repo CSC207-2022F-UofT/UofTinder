9fee72fc42d3fd27f0ebc6fe8508f9837dcb6402
Raghav Srinivasan <rsrinivasan2021@gmail.com>
2022-12-02 15:45:18 -0500

    fixed sending message from currentUser to user2

--- app/src/main/java/com/group80/uoftinder/MatchCreatorInteractor.java
+++ app/src/main/java/com/group80/uoftinder/MatchCreatorInteractor.java
@@ -1,7 +1,14 @@
 package com.group80.uoftinder;
 
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.group80.uoftinder.chat.MessageFactory;
 import com.group80.uoftinder.entities.User;
 import com.group80.uoftinder.firebase.realtime.UserRealtimeDbFacade;
+
+import com.group80.uoftinder.firebase.realtime.*;
 
 import java.util.List;
 
@@ -18,7 +25,7 @@ public class MatchCreatorInteractor {
      * profile of user2, indicating that we need to check for a match.
      *
      * If user2 has already liked currentUser, then we add the two users to each other's
-     * match list, reupload the users to the database, and send an introductory message on
+     * match list, re-upload the users to the database, and send an introductory message on
      * behalf of currentUser
      *
      * Preconditions
@@ -34,12 +41,28 @@ public class MatchCreatorInteractor {
             List<String> user2MatchList = user2.getMatches();
             user1MatchList.add(user2.getUid()); // add both users to each other's match lists
             user2MatchList.add(currentUser.getUid());
-            UserRealtimeDbFacade.uploadUser(user2); // reupload users to the database
+            UserRealtimeDbFacade.uploadUser(user2); // re-upload users to the database
             UserRealtimeDbFacade.uploadUser(currentUser);
 
-            sendIntroMessage(); // send a message from currentUser to user2
+            sendIntroMessage(currentUser.getUid(), user2.getUid(), "Hey! We matched with each other!"); // send a message from currentUser to user2
             return true;
         }
         return false;
+    }
+
+    private static void sendIntroMessage(String selfUid, String contactUid, String message) {
+        String chatRoom = selfUid.compareTo(contactUid) < 0 ? selfUid + contactUid : contactUid + selfUid;
+
+        ucChatMessageWriter chatMessageWriter = new ucChatMessageWriter(new RealtimeDbValueObserver() {
+            @Override
+            public void onRealtimeDbDataChange(@NonNull DataSnapshot snapshot) {
+            }
+
+            @Override
+            public void onRealtimeDbCancelled(@NonNull DatabaseError error) {
+            }
+        }, chatRoom);
+
+        chatMessageWriter.write(MessageFactory.createMessage(message, selfUid));
     }
 }
