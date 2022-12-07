f38f0a4137287fa6e28e411fa2201bba3772a0fa
swaner2 <j.chen1904@gmail.com>
2022-12-06 19:49:48 -0500

    refactored some code

--- app/src/main/java/com/group80/uoftinder/logout/LogOutInteractor.java
+++ app/src/main/java/com/group80/uoftinder/logout/LogOutInteractor.java
@@ -3,17 +3,11 @@ package com.group80.uoftinder.logout;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LogOutInteractor {
-    final LogOutPresenterInterface logOutPresenter;
-
-    public LogOutInteractor(LogOutPresenterInterface logOutPresenter) {
-        this.logOutPresenter = logOutPresenter;
-    }
 
     /**
-     * Signs out the user, switches UI back to LoginActivity
+     * Signs out the user
      */
     public void signOut() {
         FirebaseAuth.getInstance().signOut();
-        logOutPresenter.showLogin();
     }
 }
--- app/src/main/java/com/group80/uoftinder/logout/LogOutPresenterInterface.java
+++ app/src/main/java/com/group80/uoftinder/logout/LogOutPresenterInterface.java
@@ -1,5 +1,5 @@
 package com.group80.uoftinder.logout;
 
 public interface LogOutPresenterInterface {
-    void showLogin();
+    void signOut();
 }
