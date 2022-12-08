e41b728489579cd188e5ba39fd273fdac661d06f
swaner2 <j.chen1904@gmail.com>
2022-12-07 20:58:26 -0500

    LOGIN TESTINGGGGGGGGGGGGG

--- app/src/androidTest/java/com/group80/uoftinder/LoginTest.java
+++ app/src/androidTest/java/com/group80/uoftinder/LoginTest.java
@@ -1,24 +1,17 @@
 package com.group80.uoftinder;
 
-import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.replaceText;
-import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertNotNull;
 
-import android.util.Log;
-
-import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.group80.uoftinder.login_use_case.LoginActivity;
 
 import org.junit.Rule;
@@ -32,20 +25,17 @@ public class LoginTest {
     public ActivityScenarioRule<LoginActivity> activityRule =
             new ActivityScenarioRule<LoginActivity>(LoginActivity.class);
 
-
     @Test
     public void testTypeInEmail() {
-        String expectedEmail = "test@mail.utoronto.ca";
+        String expectedEmail = "csc207.group80.uoftinder.bot@gmail.com";
         String expectedPassword = "12345678";
 
         onView(withId(R.id.loginEmail)).perform(replaceText(expectedEmail), closeSoftKeyboard());
         onView(withId(R.id.loginPassword)).perform(replaceText(expectedPassword), closeSoftKeyboard());
         onView(withId(R.id.EnterLogin)).perform(click());
 
+        assertNotNull(FirebaseAuth.getInstance().getCurrentUser());
 
-        // Check that the text was changed.
-//        onView(withId(R.id.textToBeChanged)).check(matches(withText(STRING_TO_BE_TYPED)));
-//        onView(withText("Password:")).check(matches(isDisplayed()));
     }
 
 //    @Test
